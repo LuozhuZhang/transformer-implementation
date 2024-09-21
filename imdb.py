23c83814d5d9a8dd389ea9e2888f696bae7efc0f
Luozhu <hedgefund996@gmail.com>
2024-09-21 14:46:19 +0800

    tokenization

--- imdb.py
+++ imdb.py
@@ -19,5 +19,21 @@ dataset = load_dataset('imdb')
 train_data = dataset['train']
 test_data = dataset['test']
 
-print(train_data)
-print(test_data)
+# Step 2: Load the BERT tokenizer for tokenization
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# Function to tokenize, truncate, and pad sequences
+def tokenize_data(data, max_len=MAX_LEN):
+    tokens = tokenizer(
+        data['text'], padding='max_length', truncation=True, max_length=max_len, return_tensors='pt'
+    )
+    return tokens['input_ids'], tokens['attention_mask'], torch.tensor(data['label'])
+
+# Create dataloaders for train and test data
+def create_dataloader(dataset, batch_size=BATCH_SIZE):
+    inputs, masks, labels = tokenize_data(dataset)
+    data = torch.utils.data.TensorDataset(inputs, masks, labels)
+    return DataLoader(data, batch_size=batch_size, shuffle=True)
+
+train_loader = create_dataloader(train_data)
+test_loader = create_dataloader(test_data)
