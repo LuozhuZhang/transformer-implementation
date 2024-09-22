da7adf65df4d0122641e05a35fcf847b0f2b0d0b
Luozhu <hedgefund996@gmail.com>
2024-09-22 14:08:43 +0800

    clean up

--- encoder/imdb.py
+++ encoder/imdb.py
@@ -42,7 +42,7 @@ test_loader = create_dataloader(test_data)
 # Step 3: Define the Positional Encoding for Transformer
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
-        super(PositionalEncoding, self).__init__()
+        super().__init__()
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-np.log(10000.0) / d_model))
