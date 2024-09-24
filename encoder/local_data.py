e5076e78e3e21d0d2bad75f24a968d9b4d4744f3
Luozhu <hedgefund996@gmail.com>
2024-09-24 22:31:37 +0800

    new data set

--- encoder/local_data.py
+++ encoder/local_data.py
@@ -12,12 +12,20 @@ BATCH_SIZE = 1
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 # Define custom reviews
-custom_reviews = [
+custom_reviews_easy = [
   "I loved this movie! It was fantastic.",
   "This was the worst film I have ever seen.",
   "It was okay, nothing special.",
   "Absolutely brilliant! I would watch it again.",
   "Not my cup of tea, unfortunately."
+]
+
+custom_reviews = [
+  "Armies converge in an epic, final showdown that determines the fate of a king's dream for England - and Uhtred's hope for securing his family's future.",
+  "As a huge battle with the Danes is close, Edward has to make pivotal decisions that will affect the fates of Uhtred, Wessex - and England.",
+  "The future of England is at stake and the King refuses to hide. He is a man on a mission. Leaving the cover of the marshes, a united King Alfred and Uhtred must get word to Saxon forces; they make a rallying call to all Saxon armies to converge at Egbert's Stone to fight a decisive battle against the Danes. Treachery, deception and heartache follow, though hope remains. Wessex can still be saved, but at what cost to Uhtred?",
+  "I'll keep this short. If you are into Marvel don't watch this show. If you are into comics then move onto something else. If you are into tight well written dialog, character development and story telling then this show is a big pass.The writing in this series, so far, is convoluted, amateurish and contradictory. I ended up not caring about the characters or the story line. The acting goes from boring to over the top which given the actors involved make me think with have a major issue with the director. Who is also the writer. Go figure. There were so many missed opportunities in this first episode to make a compelling and interesting story. Another waste of money that could have been spent on something of quality.",
+  "There are lot of magical TV series/ movies which provides better pacing, thrill and a wonderful experience than Agatha all along. The first half of the first episode kept me thinking that it might be a good show. That marvel is actually back. But I am afraid to say that Marvel is still in its Mediocre era. After the first half of the 1st episode, it becomes very slow. And although I don't have much problem with that but atleast that slownessness should come with a reward. The plot is so boring that I had to watch it in parts. So, don't keep waiting for the new episodes. Watch it in 30 Oct if you want to binge and fast-forward some parts."
 ]
 
 # Function to tokenize, truncate, and pad sequences
