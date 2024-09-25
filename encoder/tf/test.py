612d0e73893aa0166144b8ac4dd13fd5b984625f
Luozhu <hedgefund996@gmail.com>
2024-09-25 17:05:47 +0800

    clean

--- encoder/tf/test.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset('imdb')
-train_data = dataset['train']
-test_data = dataset['test']
-
-for i in range(20):
-  print(train_data[i]['text'])
